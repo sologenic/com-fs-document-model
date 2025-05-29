a771e01b37445bb94c2f738c498e5d0f82500edc
“jayjang0209” <jayjang0209@gmail.com>
2025-05-29 13:48:42 -0700

    feat: document model

--- bin/build.sh
+++ bin/build.sh
@@ -19,14 +19,6 @@ protoc \
     --go-grpc_opt=require_unimplemented_servers=false \
     "--go-grpc_out=." --go-grpc_opt=paths=source_relative
 
-protoc \
-    --proto_path=. "document-requests.proto" \
-    --proto_path=$(dirname $(dirname "$rd")) \
-    "--go_out=." --go_opt=paths=source_relative \
-    --go-grpc_opt=require_unimplemented_servers=false \
-    "--go-grpc_out=." --go-grpc_opt=paths=source_relative
-
-
 # if there's TS project in the package, generate a protobuf file for TS
 rm -rf node_modules
 npm i
@@ -36,7 +28,6 @@ protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
     --proto_path=$(dirname $(dirname "$rd")) \
     --ts_proto_out=. \
     --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
     document.proto
 
 npm run build
